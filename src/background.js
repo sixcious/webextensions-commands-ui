6e8e3a8aa909bc775623c4138348eec91cf41941
roysix <roysix@users.noreply.github.com>
2018-09-10 11:12:43 -0400

    .

---
+++ src/background.js
@@ -0,0 +1,36 @@
+/**
+ * Listen for commands (Browser Extension shortcuts) and perform the command's action.
+ *
+ * @param command the shortcut command that was performed
+ * @public
+ */
+function commandListener(command) {
+  console.log ("WebExtensionsCommandsUIDemo - command=" + command);
+  browser.browserAction.setIcon({ "path": { "16": "/img/" + command + "/16.png"}});
+}
+
+/**
+ * Listen for installation changes and do initialization work.
+ *
+ * @param details the installation details
+ * @public
+ */
+function installedListener(details) {
+  if (details.reason === "install") {
+    console.log("WebExtensionsCommandsUIDemo - details=" + details);
+    browser.runtime.openOptionsPage();
+  }
+}
+
+// Chrome: Compatibility to recognize browser namespace
+if (typeof browser === "undefined") {
+  browser = chrome;
+}
+
+// Installed Listener
+browser.runtime.onInstalled.addListener(installedListener);
+
+// Firefox Android: chrome.commands is unsupported
+if (browser.commands && browser.commands.onCommand) {
+  browser.commands.onCommand.addListener(commandListener);
+}
